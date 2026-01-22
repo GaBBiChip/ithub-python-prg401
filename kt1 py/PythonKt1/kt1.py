f8c313ee69839cb0565db8b8bbcdb98ddc7d7b5e
GaBBiChip <ruslanfaiz12@gmail.com>
2026-01-22 17:40:02 +0700

    Add files via upload

---
+++ kt1 py/PythonKt1/kt1.py
@@ -0,0 +1,16 @@
+text = input()
+n = len(text) + 4
+
+i = 0
+while i < n:
+    print('*')
+    i += 1
+print()
+
+print('* ' + text + ' *')
+
+i = 0
+while i < n:
+    print('*')
+    i += 1
+print()
